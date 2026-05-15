3962aaedadc2ec11a41fefc786a76c4a45d9bd20
averyluxg <alg190000@utdallas.edu>
2026-05-14 23:33:16 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
 // main.js
 
-function randomNumber(min, max){
-return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-let divs = document.querySelectorAll(".grid-container div");
-function randomize(){
-console.log(divs.length, "randomize! ")
-}
+document.body.style.opacity = .5;
